94e2dd4878233f98d8d0961c9e58caac7e512526
Zeinab-Ali <zoza.aly200@gmail.com>
2022-10-06 00:56:43 +0200

    add task2

--- task2/table.php
+++ task2/table.php
@@ -1,46 +1,124 @@
+<?php
 
-  <?php  
 
-  ?>
-   
-               
-<!doctype html>
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+        
+
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+       
+
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+        
+    ],
+
+];
+
+$header = '';
+
+$row = '';
+
+foreach ($users[0] as $property => $value) {
+    $header .= '<th>' . $property . '</th>';
+}
+
+for ($i = 0; $i < count($users); $i++) {
+    $col = '';
+    foreach ($users[$i] as $property => $value) {
+       
+        if ($property == 'id') {
+            $col .= '<td>' . $value . '</td>';
+        }
+
+        if (is_string($value)) {
+            $col .= '<td>' . $value . '</td>';
+        } else if (is_array($value) || is_object($value)) {
+           
+            $colval = '';
+            foreach ($value as $key => $val) {
+                if ($val == 'm') {
+                    $colval .= 'Male';
+                } else if ($val == 'f') {
+                    $colval .= 'Female';
+                }
+                else  {
+
+                    $colval .= $val . ', <br>';
+                } 
+                
+            }
+            $col .= '<td>' . $colval . '</td>';
+           
+        }
+      
+    }
+    $row .= '<tr>' . $col . '</tr>';
+}
+
+
+
+$table = '<table class="table table-hover">' . $header . $row . '</table>';
+?>
+
+
+<!DOCTYPE HTML>
+
 <html lang="en">
-  <head>
-    <title>Task1</title>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-       <div class="contianer border-3 col-8 border border-danger mx-auto rounded"> 
-           <table class=" 
-           table  text-center  mt-5">
-                 <thead class="thead-dark">
-                        <tr>
-                             <th scope="col">ID</th>
-                             <th scope="col">NAME</th>
-                             <th scope="col">GENDER</th>
-                             <th scope="col">HOPPIES</th> 
-                             <th scope="col">ACTIVITES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                         <tr>
-                                <td scope="row"></td>
-                                <td></td>
-                                <td></td>
-                        </tr>
-                        
-                    </tbody>
-            </table>
-       </div> 
-  
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Dynamic Table</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <h2 class="text-center text-primary m-5">Dynamic Table</h2>
+        <?php print_r($table) ?>
+    </div>
+</body>
+
 </html>
